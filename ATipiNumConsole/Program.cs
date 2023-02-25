562495bbc309db9241e5c74e675db4e3682021c7
cristina.ravacchioli <cristina.ravacchioli@gmail.com>
2023-02-25 09:18:09 +0100

    Add project files.

---
+++ ATipiNumConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ATipiNumConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float prezzoIntero = 100.0F;
+            float sconto = 0.1F;
+            float prezzoFinale = 0;
+
+            Console.WriteLine($"FLOAT Prezzo iniziale: {prezzoIntero} ");
+            for (int i = 1; i < 10; i++)
+            {
+                prezzoFinale = prezzoIntero * (1 - i * sconto);
+                Console.WriteLine($"sconto {i}0% ->Prezzo scontato {prezzoFinale} ");
+            }
+
+
+
+        }
+    }
+}
